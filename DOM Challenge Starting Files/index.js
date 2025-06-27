c5c45e5768b00821813c4436e4e5fbf034cc5284
balbir <prasadbalbir1056@gmail.com>
2025-06-27 10:22:16 +0530

    DOM challenge 3

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -4,3 +4,6 @@ console.log(lastElement.innerHTML);
 lastElement.innerHTML = "Last Element Changed";
 let btn = document.querySelector("button");
 btn.style.backgroundColor = "yellow";
+
+let heading = document.querySelector("h1");
+heading.classList.add("huge");
