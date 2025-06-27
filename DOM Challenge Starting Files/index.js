548a37285e28dfdc7f45a8a58e93c3aa84662997
balbir <prasadbalbir1056@gmail.com>
2025-06-27 10:05:43 +0530

    DOM challenge 2

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -2,3 +2,5 @@ let lst = document.querySelector("ul");
 let lastElement = lst.lastElementChild;
 console.log(lastElement.innerHTML);
 lastElement.innerHTML = "Last Element Changed";
+let btn = document.querySelector("button");
+btn.style.backgroundColor = "yellow";
