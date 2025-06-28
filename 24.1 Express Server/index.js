d7846cd6e124b11cd7e84ea57db200477bbd3e4a
balbir <prasadbalbir1056@gmail.com>
2025-06-28 12:39:34 +0530

    24.1: other http methods

--- 24.1 Express Server/index.js
+++ 24.1 Express Server/index.js
@@ -14,6 +14,17 @@ app.get("/contact", (req, res) => {
   res.send("<h1>Contact Page</h1>");
 });
 
+app.put("/update", (req, res) => {
+  res.send("<h1>Update Page</h1>");
+});
+
+app.delete("/delete", (req, res) => {
+  res.send("<h1>Delete Page</h1>");
+});
+
+app.post("/create", (req, res) => {
+  res.send("<h1>Create Page</h1>");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
