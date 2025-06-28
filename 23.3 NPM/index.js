3c7b7ecb1617600e73d3307c571684efc6ba20d7
balbir <prasadbalbir1056@gmail.com>
2025-06-28 09:25:20 +0530

    23.3: importing functions

--- 23.3 NPM/index.js
+++ 23.3 NPM/index.js
@@ -1,4 +1,7 @@
 import generateRandom from "sillyname";
+import { randomSuperhero as superHeroName } from "superheroes";
 
 let myName = generateRandom();
+let supHero = superHeroName();
 console.log(myName);
+console.log("I am a superhero: " + supHero);
