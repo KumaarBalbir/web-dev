ec480ac82d787dabd7b59e0af6003c348bd31dc0
balbir <prasadbalbir1056@gmail.com>
2025-06-28 08:35:46 +0530

    23.2: read data from a file

--- 23.2 Native Modules/index.js
+++ 23.2 Native Modules/index.js
@@ -3,3 +3,8 @@ fs.writeFile("output.txt", "Hail Hitler!", (err) => {
   if (err) throw err;
   console.log("Message written to file.");
 });
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
