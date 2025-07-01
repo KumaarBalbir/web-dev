8c1234d04738db968845ac16fdc25573fc8ed4f2
balbir <prasadbalbir1056@gmail.com>
2025-07-01 17:57:44 +0530

    26.2: api authn - no auth request

--- 26.2 API Authentication/index.js
+++ 26.2 API Authentication/index.js
@@ -1,24 +1,30 @@
 import express from "express";
 import axios from "axios";
 
+const uname = "kumaar";
+const password = "iambalbir";
+const myapikey = "474b2e86-5bcd-4bd1-97ba-5d255bad4ca9";
+const myauthTOken = "474b2e86-5bcd-4bd1-97ba-5d255bad4ca9";
 const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com/";
 
 //TODO 1: Fill in your values for the 3 types of auth.
-const yourUsername = "";
-const yourPassword = "";
-const yourAPIKey = "";
-const yourBearerToken = "";
+const yourUsername = uname;
+const yourPassword = password;
+const yourAPIKey = myapikey;
+const yourBearerToken = myauthTOken;
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
-app.get("/noAuth", (req, res) => {
+app.get("/noAuth", async (req, res) => {
   //TODO 2: Use axios to hit up the /random endpoint
   //The data you get back should be sent to the ejs file as "content"
   //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
+  let content = await fetchNoAuth(`${API_URL}random`);
+  res.render("index.ejs", { content: content });
 });
 
 app.get("/basicAuth", (req, res) => {
@@ -55,6 +61,18 @@ app.get("/bearerToken", (req, res) => {
   });
   */
 });
+
+async function fetchNoAuth(url) {
+  try {
+    const response = await axios.get(url);
+    // console.log("response data: ", response.data);
+    const data = JSON.stringify(response.data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    return { content: "Error fetching data." };
+  }
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
