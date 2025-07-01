e9b30f0b5d165706f9331b52fff05f3729cd256b
balbir <prasadbalbir1056@gmail.com>
2025-07-01 09:58:30 +0530

    25.5: mount static css files

--- 25.4 Band Generator Project/index.js
+++ 25.4 Band Generator Project/index.js
@@ -12,6 +12,9 @@ const port = 3000;
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
 
