17e595c1d0755709ecb2cb7424d4fb8d00adb120
balbir <prasadbalbir1056@gmail.com>
2025-07-03 17:28:43 +0530

    27.1: replace old body-parser middleware with express midlleware

--- 27.1 Blog API Project/index.js
+++ 27.1 Blog API Project/index.js
@@ -35,8 +35,9 @@ let posts = [
 let lastId = 3;
 
 // Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Write your code here//
 
