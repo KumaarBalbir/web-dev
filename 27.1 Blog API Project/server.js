06a6e349eddd67c8966884d9413cf6827c43e2ca
balbir <prasadbalbir1056@gmail.com>
2025-07-03 17:30:11 +0530

    27.1: remove body-parser and add express in built middleware

--- 27.1 Blog API Project/server.js
+++ 27.1 Blog API Project/server.js
@@ -1,15 +1,14 @@
 import express from "express";
-import bodyParser from "body-parser";
 import axios from "axios";
-
 const app = express();
 const port = 3000;
 const API_URL = "http://localhost:4000";
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Route to render the main page
 app.get("/", async (req, res) => {
