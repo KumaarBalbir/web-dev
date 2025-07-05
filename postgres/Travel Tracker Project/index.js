3005f79f343efccf4b03c15f148c76986ce4ca2b
balbir <prasadbalbir1056@gmail.com>
2025-07-05 12:55:22 +0530

    travel: add country from client to database (persist)

--- postgres/Travel Tracker Project/index.js
+++ postgres/Travel Tracker Project/index.js
@@ -6,8 +6,6 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-let visited_countries = [];
-
 const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -16,16 +14,6 @@ const db = new pg.Client({
   port: process.env.DB_PORT,
 });
 await db.connect();
-try {
-  const res = await db.query("SELECT * FROM visited_countries");
-  visited_countries = res.rows.map((row) => row.country_code);
-  console.log("Fetched visited countries from database: ", visited_countries);
-} catch (error) {
-  console.error("Error executing queries: ", error.stack);
-} finally {
-  await db.end();
-  console.log("Database connection closed.");
-}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,10 +21,46 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  let visited_countries = [];
+  try {
+    const result = await db.query("SELECT * FROM visited_countries");
+    visited_countries = result.rows.map((row) => row.country_code);
+    // console.log("Fetched visited countries from database: ", visited_countries);
+  } catch (error) {
+    console.error("Error executing queries: ", error.stack);
+  }
+
   res.render("index.ejs", {
     countries: visited_countries,
     total: visited_countries.length,
   });
+});
+
+app.post("/add", async (req, res) => {
+  console.log("recieved request", req.body);
+  let country_name = req.body.country;
+  country_name = country_name.trim().toLowerCase();
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) = $1",
+      [country_name]
+    );
+    const country_code = result.rows[0]?.country_code;
+    if (!country_code) {
+      console.error("Country not found in database: ", country_name);
+      return res.status(404).send("Country not found");
+    }
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      country_code,
+    ]);
+
+    console.log("Added country to database: ", country_code);
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error adding country to database: ", error.stack);
+    res.status(500).send("Error adding country");
+  }
 });
 
 app.listen(port, () => {
