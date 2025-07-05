a965090b0446e8c35c211500085bed13ff9edd24
balbir <prasadbalbir1056@gmail.com>
2025-07-05 13:26:49 +0530

    travel: support wildcard country search functionality

--- postgres/Travel Tracker Project/index.js
+++ postgres/Travel Tracker Project/index.js
@@ -43,7 +43,7 @@ app.post("/add", async (req, res) => {
 
   try {
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) = $1",
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'",
       [country_name]
     );
     let visited_countries = await getVisitedCountries();
