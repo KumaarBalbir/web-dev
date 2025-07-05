28d6a092bd1783e716f18378949732dd1424cc4a
balbir <prasadbalbir1056@gmail.com>
2025-07-05 12:00:06 +0530

    travel: use express inbuilt middleware

--- postgres/Travel Tracker Project/index.js
+++ postgres/Travel Tracker Project/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
