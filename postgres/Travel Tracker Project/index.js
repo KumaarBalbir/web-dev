d78740bf51ebccf5fd6d9ab856880900abf27474
balbir <prasadbalbir1056@gmail.com>
2025-07-05 12:11:14 +0530

    travel: show visted country on map

--- postgres/Travel Tracker Project/index.js
+++ postgres/Travel Tracker Project/index.js
@@ -1,7 +1,31 @@
 import express from "express";
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
+
+let visited_countries = [];
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+await db.connect();
+try {
+  const res = await db.query("SELECT * FROM visited_countries");
+  visited_countries = res.rows.map((row) => row.country_code);
+  console.log("Fetched visited countries from database: ", visited_countries);
+} catch (error) {
+  console.error("Error executing queries: ", error.stack);
+} finally {
+  await db.end();
+  console.log("Database connection closed.");
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +33,10 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  res.render("index.ejs", {
+    countries: visited_countries,
+    total: visited_countries.length,
+  });
 });
 
 app.listen(port, () => {
