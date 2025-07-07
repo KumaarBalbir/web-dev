9fa7094940acab53bc5221788c0bb784388acffa
balbir <prasadbalbir1056@gmail.com>
2025-07-07 10:41:09 +0530

    auth lv1: initial files

---
+++ postgres/Authentication Lv1/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {});
+
+app.post("/login", async (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
