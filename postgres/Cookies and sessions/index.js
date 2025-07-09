f2d5960cb9074fdcb25be1625b55869b90399a8b
balbir <prasadbalbir1056@gmail.com>
2025-07-09 09:57:23 +0530

    auth lv3: code comment for local strategy

--- postgres/Cookies and sessions/index.js
+++ postgres/Cookies and sessions/index.js
@@ -40,6 +40,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Passport Local Strategy for user authentication
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
