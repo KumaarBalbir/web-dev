03c856ccd7dbae9d84b172168bc8bbc05cdfb5c7
balbir <prasadbalbir1056@gmail.com>
2025-07-10 10:02:29 +0530

    secrets project: implements get submit route

--- postgres/Secrets Project/index.js
+++ postgres/Secrets Project/index.js
@@ -75,6 +75,13 @@ app.get("/secrets", async (req, res) => {
 
 //TODO: Add a get route for the submit button
 //Think about how the logic should work with authentication.
+app.get("/submit", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("submit.ejs");
+  } else {
+    res.redirect("/login");
+  }
+});
 
 app.get(
   "/auth/google",
